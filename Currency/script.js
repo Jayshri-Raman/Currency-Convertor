b0c8eb70c30ae06f94f22662a0414b300a2b3b26
Jayshri Raman <138697903+Jayshri-Raman@users.noreply.github.com>
2024-06-18 22:04:04 +0530

    Add files via upload

---
+++ Currency/script.js
@@ -0,0 +1,41 @@
+fetch(`https://api.frankfurter.app/currencies`).then(res=>res.json())
+.then(res=>display(res))
+let select = document.querySelectorAll('.currency')
+console.log(select)
+function display(res)
+{
+	
+	let curr=Object.entries(res)
+	for(let i=0;i<curr.length;i++)
+	{
+		let opt=`<option value="${curr[i][0]}">${curr[i][0]}</option>`
+		select[0].innerHTML+=opt;
+		select[1].innerHTML+=opt;
+		
+	}
+}
+let input=document.getElementById('input')
+
+let btn=document.getElementById('btn')
+btn.addEventListener('click',()=>{
+let curr1=select[0].value;
+let curr2=select[1].value;
+let inputVal=input.value;
+if(curr1==curr2)
+		{
+			alert("Invalid")
+		}
+		else{
+			convert(curr1,curr2,inputVal)
+		}
+
+})
+
+function convert(curr1,curr2,inputVal){
+	const host = 'api.frankfurter.app';
+fetch(`https://${host}/latest?amount=${inputVal}&from=${curr1}&to=${curr2}`)
+  .then(resp => resp.json())
+  .then((data) => {
+    document.getElementById('result').value=Object.values(data.rates)[0];
+  });
+}
